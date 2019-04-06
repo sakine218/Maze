c9e9035bc853bd15bf0d543e72c925d68eaa10ad
sakine218 <snishibayashi218@gmail.com>
2019-04-06 15:00:32 +0900

    second commit

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let screenSize = UIScreen.main.bounds.size
+    
+    let maze = [
+        [1, 0, 0, 0, 1, 0],
+        [1, 0, 1, 0, 1, 0],
+        [3, 0, 1, 0, 1, 0],
+        [1, 1, 1, 0, 0, 0],
+        [1, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1],
+        [0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 2],
+    ]
 
+    var startView: UIView!
+    var goalView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let cellWidth = screenSize.width / CGFloat(maze[0].count)
+        let cellHeight = screenSize.height / CGFloat(maze.count)
+        
+        let cellOffsetX = screenSize.width / CGFloat(maze[0].count * 2)
+        let cellOffsetY = screenSize.height / CGFloat(maze.count * 2)
+        
+        for y in 0 ..< maze.count {
+            //コード書く
+        }
+    }
+    
+    func createView(x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat, offsetY: CGFloat) -> UIView {
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        let view = UIView(frame: rect)
+        
+        let center = CGPoint(x: offsetX + width * CGFloat(x), y: offsetY + height * CGFloat(y))
+        view.center = center
+        
+        return view
     }
 
 
